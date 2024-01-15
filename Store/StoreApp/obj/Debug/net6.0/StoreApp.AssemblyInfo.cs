0dd39ee48aa01fe1bc8155dbd5831e5c42dc23a4
Kadircan UZAN <kadircanuzan@gmail.com>
2024-01-15 12:27:28 +0300

    ViewComponent ile birlikte ürün sayılarının toplamınına ulaşacağımız bir sınıf oluşturuldu.

--- Store/StoreApp/obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
+++ Store/StoreApp/obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3341cced7e96cdbe94cca7138946a9cf626ec0f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1acada706b3b431801b9b20bf0301c4c756a472f")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

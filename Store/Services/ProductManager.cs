7a36c520a155346e7f172639a32cb4dbe5c77ee7
Kadircan UZAN <kadircanuzan@gmail.com>
2024-01-15 17:58:56 +0300

    ViewComponent ile birlikte Left Bar Menü cshtml sayfası oluşturuldu.

---
+++ Store/Services/ProductManager.cs
@@ -0,0 +1,31 @@
+﻿using Repositories.Contracts;
+using Services.Contracts;
+using Store.Entities.Models;
+
+namespace Services
+{
+    public class ProductManager : IProductService
+    {
+        private readonly IRepositoryManager _manager;
+
+        public ProductManager(IRepositoryManager repositoryManager)
+        {
+            _manager = repositoryManager;
+        }
+
+        public IEnumerable<Product> GetAllProducts(bool trackChanges)
+        {
+           return _manager.Product.GetAllProducts(trackChanges);
+        }
+
+        public Product? GetOneProduct(int id, bool trackChanges)
+        {
+            var product = _manager.Product.GetOneProduct(id, trackChanges);
+            if (product is null)
+            {
+                throw new Exception("Product not found");
+            }
+            return product;
+        }
+    }
+}
---
+++ Store/StoreApp/Components/ProductSummaryViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+namespace StoreApp.Components
+{
+    public class ProductSummaryViewComponent : ViewComponent
+    {
+        private readonly IServiceManager _manager;
+
+        public ProductSummaryViewComponent(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        //Productların sayısını görmek istediğimiz metot 
+        public string Invoke()
+        {
+            return _manager.ProductService.GetAllProducts(false).Count().ToString();
+        }
+    }
+}

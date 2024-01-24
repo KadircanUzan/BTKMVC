748d6793c934963352bfdad2bb908ff1cfeb310c
Kadircan UZAN <kadircanuzan@gmail.com>
2024-01-25 00:54:47 +0300

    Product sınıfı içerisine Summary özelliği atandı.

--- Store/Entities/Models/Product.cs
+++ Store/Entities/Models/Product.cs
@@ -7,6 +7,7 @@ namespace Entities.Models
         public int ProductId { get; set; }
         public string? ProductName { get; set; } = string.Empty;
         public decimal Price { get; set; }
+        public string Summary { get; set; } = string.Empty;
         public int? CategoryId { get; set; }    // Foreign Key
         public Category? Category { get; set; } // Navigation property
     }
